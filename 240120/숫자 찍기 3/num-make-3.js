bf8f091344e85b1c81d4df13684dcf25e3b30bdb
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-01-20 15:52:24 +0900

    Title: 숫자 찍기 3, Time: 230ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240120/숫자 찍기 3/num-make-3.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+
+const [n, type] = input[0].split(" ").map(Number)
+let result = [];
+
+for (let i = 0; i < n; i++) {
+    result[i] = [];
+    for (let j = 0; j <= i; j++) {
+        if (j === 0 || j === i) {
+            result[i][j] = 1;
+        } else {
+            result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
+        }
+    }
+}
+const arr = result.reverse()
+
+if (type === 1) {
+    result.forEach(row => console.log(row.join(' ')))
+} else if (type === 2) {
+    arr.forEach((row, i) => {
+        console.log(' '.repeat(i) + row.join(' ') + ' '.repeat(i))
+    })
+} else if (type === 3) {
+    const newArr = []
+
+    for (let i = 0; i < arr.length; i++) {
+        const row = []
+        for (let j = 0; j < arr[i].length; j++) {
+            row.push(arr[j][i])
+        }
+        newArr.push(row)
+    }
+    newArr.reverse().forEach(row => console.log(row.join(' ')))
+}
