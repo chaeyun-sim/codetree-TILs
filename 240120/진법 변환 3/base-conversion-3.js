50c4d0255c16ad4cb8ecb7b58c42e8c2ccb3301b
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-01-20 16:29:00 +0900

    Title: 진법 변환 3, Time: 247ms, Memory: 27MB, Status: Passed - CodeTree

--- 240120/진법 변환 3/base-conversion-3.js
+++ 240120/진법 변환 3/base-conversion-3.js
@@ -2,12 +2,12 @@ const fs = require('fs')
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
 const octal = input[0]
-let binary = '';
-  
-for (let i = 0; i < octal.length; i++) {
-    const digit = parseInt(octal[i], 8);
-    const binaryDigit = digit.toString(2).padStart(3, '0');
-    binary += binaryDigit;
-}
+  let binary = '';
 
-console.log(Number(binary))
+  for (let i = 0; i < octal.length; i++) {
+    const digit = parseInt(octal[i], 8);
+    const binaryDigit = digit.toString(2);
+    binary += i > 0 ? binaryDigit.padStart(3, '0') : binaryDigit;
+  }
+
+  console.log(binary)
