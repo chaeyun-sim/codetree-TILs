0c88ff1f9097064c20704e2e87fa04cb7e397eca
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-01-24 18:37:42 +0900

    Title: 세 수의 중간값, Time: 196ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240124/세 수의 중간값/median-of-three-numbers.js
@@ -0,0 +1,7 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+// 세 수 a, b, c의 값이 주어졌을 때, b가 a보다 크고 c보다 작다면 1을, 그렇지 않다면 0을 출력하는 프로그램을 작성해보세요.
+
+const [a, b, c] = input[0].split(" ").map(Number)
+
+console.log(+(b > a && a < c))
