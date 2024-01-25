749a629a4dba95ef847fb54492ae2f1ad15ae565
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-01-25 14:08:54 +0900

    Title: 두 사람, Time: 247ms, Memory: 47MB, Status: Passed - CodeTree

--- 240125/두 사람/two-person.js
+++ 240125/두 사람/two-person.js
@@ -1,5 +1,14 @@
 const fs = require('fs')
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
-const arr = input.map(el => el.split(' ')).filter(el => el[0] >= 19 && el[1] === 'M')
-console.log(arr.length)
+let cnt = 0;
+let flag = false;
+input.forEach(item => {
+    const [age, gender] = item.split(' ');
+
+    if (age >= 19 && gender === 'M') {
+        flag = true
+    }
+})
+
+console.log(+(flag))
