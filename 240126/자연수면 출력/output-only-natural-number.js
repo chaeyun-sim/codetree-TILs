92f7a6045c6c954968cb02357e2d84393d0aef4a
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-01-26 18:32:58 +0900

    Title: 자연수면 출력, Time: 204ms, Memory: 14MB, Status: Passed - CodeTree

--- 240126/자연수면 출력/output-only-natural-number.js
+++ 240126/자연수면 출력/output-only-natural-number.js
@@ -2,4 +2,10 @@ const fs = require('fs')
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
 const [a, b] = input[0].split(' ').map(Number)
-console.log(a.toString().repeat(b))
+let result = ''
+
+for (let i = 0; i < b; i++) {
+    result += a
+}
+
+console.log(a <= 0 ? 0 : result)
