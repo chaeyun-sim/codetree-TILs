ec2db2fb491c1a79a8ce307b2ef09e393a4dc5c9
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-01-30 19:20:16 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 303ms, Memory: 14MB, Status: Passed - CodeTree

--- 240130/2가 3번째로 등장하는 위치/where-2-appears-3rd.js
+++ 240130/2가 3번째로 등장하는 위치/where-2-appears-3rd.js
@@ -5,10 +5,10 @@ const arr = input[1].split(' ').map(Number)
 let cnt = 0;
 
 for (let i = 0; i < arr.length; i++) {
-    if (cnt == 3) {
-        console.log(i)
+    if (arr[i] === 2) cnt++
+
+    if (cnt === 3) {
+        console.log(i + 1)
         return;
     }
-
-    if (arr[i] === 2) cnt++
 }
