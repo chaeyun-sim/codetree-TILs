dd552ec156477ab44560f2a132490f9c75e80e35
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-04 21:56:14 +0900

    Title: 각 자리 숫자의 제곱, Time: 231ms, Memory: 14MB, Status: Passed - CodeTree

--- 240204/각 자리 숫자의 제곱/square-of-each-digit.js
+++ 240204/각 자리 숫자의 제곱/square-of-each-digit.js
@@ -3,9 +3,18 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
 const n = input[0]
 
-function solution(i) {
-    if (i === n.length - 1) return Number(n[n.length - 1]);
-    return (Number(n[i]) ** 2) + solution(i + 1)
+function sumOfSquaresDigits(inputStr) {
+    if (inputStr.length === 0) {
+        return 0;
+    }
+    
+    const lastDigit = parseInt(inputStr.slice(-1));
+    const lastDigitSquare = lastDigit * lastDigit;
+    const remainingDigitsSum = sumOfSquaresDigits(inputStr.slice(0, -1));
+    
+    return lastDigitSquare + remainingDigitsSum;
 }
 
-console.log(solution(0))
+const result = sumOfSquaresDigits(n);
+
+console.log(result);
