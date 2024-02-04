0c94f8ec0c27f0ae43630a368a7f84e9e35d8c79
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-05 02:03:23 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 223ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240205/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.js
+++ 240205/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.js
@@ -3,12 +3,17 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
 const n = Number(input[0])
 let cnt = 0
+const arr = Array(n + 1).fill(0)
 
-function solution (num) {
-    if (cnt === n) return;
-    cnt++
-    return solution(num - 2) + solution(num - 1)
+function fibonacci(num) {
+    cnt++;
+    if (num <= 1) return num;
+
+    if (arr[num] !== 0) return arr[num];
+
+    arr[num] = fibonacci(num - 1) + fibonacci(num - 2);
+    return arr[num];
 }
 
-solution(1)
-console.log(cnt - 1)
+fibonacci(n);
+console.log(arr[n])
