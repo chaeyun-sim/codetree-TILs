9cf3a6d1f7886b29668f99610befa17a1adc987f
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-04 21:49:17 +0900

    Title: 각 자리 숫자의 제곱, Time: 178ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240204/각 자리 숫자의 제곱/square-of-each-digit.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+
+const n = input[0]
+
+function solution(i) {
+    if (i === n.length - 1) return 1;
+    return (Number(n[i]) ** 2) + solution(i + 1)
+}
+
+console.log(solution(0))
