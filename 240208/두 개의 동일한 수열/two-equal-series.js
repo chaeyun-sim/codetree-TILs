9e629ea3954faf06dd4245755fcdd6710773b590
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-08 13:38:54 +0900

    Title: 두 개의 동일한 수열, Time: 184ms, Memory: 14MB, Status: Passed - CodeTree

--- 240208/두 개의 동일한 수열/two-equal-series.js
+++ 240208/두 개의 동일한 수열/two-equal-series.js
@@ -1,24 +1,16 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const a = input[1].split(' ').map(Number)
-const b = input[2].split(' ').map(Number)
+const a = input[1].split(' ').filter(el => el)
+const b = input[2].split(' ').filter(el => el)
 
-function equal () {
+function isEqualSet() {
     for (let i = 0; i < a.length; i++) {
-        if (a[i] !== b[i]) {
-            return false;
+        if (!b.includes(a[i])) {
+            return false 
         }
     }
-
-    return true;
+    return true
 }
 
-a.sort((a, b) => a - b)
-b.sort((a, b) => a - b)
-
-if (equal()) {
-    console.log('Yes')
-} else {
-    console.log('No')
-}
+console.log(isEqualSet() ? 'Yes' : 'No');
