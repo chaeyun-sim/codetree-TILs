fa74646f2670175e1285894bd3c1e0564c7739e2
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-10 15:16:45 +0900

    Title: 그 요일은, Time: 232ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240210/그 요일은/the-day-of-the-day.js
+++ 240210/그 요일은/the-day-of-the-day.js
@@ -1,29 +1,25 @@
-const fs = require('fs')
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const [m1, d1, m2, d2] = input[0].split(' ').map(Number)
-const target = input[1]
-const daysByMonth = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+const [m1, d1, m2, d2] = input[0].split(' ').map(Number);
+const target = input[1];
+const daysByMonth = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
-
 const calculateDays = (m, d) => {
-    let totalDays = 0
-
+    let totalDays = 0;
     for (let i = 1; i < m; i++) {
-        totalDays += daysByMonth[m]
+        totalDays += daysByMonth[i];
     }
+    return totalDays + d;
+};
 
-    return totalDays + d
-}
-
-const date = calculateDays(m2, d2) - calculateDays(m1, d1)
+const dateDifference = calculateDays(m2, d2) - calculateDays(m1, d1);
 
 const startDayIndex = days.indexOf('Mon');
 let targetDayIndex = days.indexOf(target);
 
-let diff = targetDayIndex - startDayIndex;
-if (diff < 0) diff += 7;
+let diff = (targetDayIndex - startDayIndex + 7) % 7;
+const count = Math.floor((dateDifference + diff) / 7);
 
-const count = Math.floor((date - diff) / 7) + 1;
-console.log(count)
+console.log(count);
