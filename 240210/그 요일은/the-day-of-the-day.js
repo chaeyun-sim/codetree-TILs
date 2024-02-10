3d3f6a826ad6fd7e49392f5e6624a759938087cc
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-10 15:14:25 +0900

    Title: 그 요일은, Time: 178ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240210/그 요일은/the-day-of-the-day.js
+++ 240210/그 요일은/the-day-of-the-day.js
@@ -2,7 +2,10 @@ const fs = require('fs')
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
 const [m1, d1, m2, d2] = input[0].split(' ').map(Number)
+const target = input[1]
 const daysByMonth = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
 
 const calculateDays = (m, d) => {
     let totalDays = 0
@@ -16,4 +19,11 @@ const calculateDays = (m, d) => {
 
 const date = calculateDays(m2, d2) - calculateDays(m1, d1)
 
-console.log(Math.ceil(date / 7))
+const startDayIndex = days.indexOf('Mon');
+let targetDayIndex = days.indexOf(target);
+
+let diff = targetDayIndex - startDayIndex;
+if (diff < 0) diff += 7;
+
+const count = Math.ceil((date - diff) / 7) + 1;
+console.log(count)
