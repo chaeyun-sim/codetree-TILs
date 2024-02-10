4f7c8eac4d47aada3932ddd59595f52f8742fa45
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-10 14:35:16 +0900

    Title: Date to Date, Time: 180ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240210/Date to Date/date-to-date.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+
+const days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+let [m1, d1, m2, d2] = input[0].split(' ').map(Number)
+let total = 0
+
+while (true) {
+    if (m1 === m2 && d1 === d2) break;
+
+    d1++
+    total++
+
+    if (d1 === days[m1]) {
+        m1++
+        d1 = 1
+    }
+}
+
+console.log(total + 2)
