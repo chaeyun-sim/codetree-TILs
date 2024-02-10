e8f023c677e9a0f235b6d7dd3aa35e49449b6dc8
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-10 15:20:59 +0900

    Title: 그 요일은, Time: 229ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240210/그 요일은/the-day-of-the-day.js
+++ 240210/그 요일은/the-day-of-the-day.js
@@ -6,20 +6,21 @@ const target = input[1];
 const daysByMonth = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
-const calculateDays = (m, d) => {
+const calculateDaysFromYearStart = (m, d) => {
     let totalDays = 0;
     for (let i = 1; i < m; i++) {
         totalDays += daysByMonth[i];
     }
-    return totalDays + d;
+    totalDays += d;
+    return totalDays;
 };
 
-const dateDifference = calculateDays(m2, d2) - calculateDays(m1, d1);
+const daysBetween = calculateDaysFromYearStart(m2, d2) - calculateDaysFromYearStart(m1, d1);
 
-const startDayIndex = days.indexOf('Mon');
-let targetDayIndex = days.indexOf(target);
+const startIndex = days.indexOf('Mon');
+const targetIndex = days.indexOf(target);
+const dayDiff = (targetIndex - startIndex + 7) % 7;
 
-let diff = (targetDayIndex - startDayIndex + 7) % 7;
-const count = Math.floor((dateDifference + diff) / 7);
+let count = Math.floor((daysBetween + dayDiff) / 7);
 
 console.log(count);
