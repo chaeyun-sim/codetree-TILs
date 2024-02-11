22a246e188612e9d79a66e259e2b973aa15e0a8d
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-11 16:47:02 +0900

    Title: 진수 to 진수, Time: 202ms, Memory: 16MB, Status: Passed - CodeTree

--- 240211/진수 to 진수/transformation-of-number-system.js
+++ 240211/진수 to 진수/transformation-of-number-system.js
@@ -1,19 +1,18 @@
-const fs = require('fs')
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const [a, b] = input[0].split(' ').map(Number)
-const n = input[1]
-let decimal = 0
+const [a, b] = input[0].split(' ').map(Number);
+const n = input[1];
+let decimal = 0;
 
-for (let i = 0; i < String(n).length; i++) {
-    decimal += a ** i * n[n.length - 1 - i]
+for (let i = 0; i < n.length; i++) {
+    decimal += parseInt(n[i], a) * (a ** (n.length - 1 - i));
 }
 
 let result = '';
-
 while (decimal > 0) {
-    result += (decimal % b)
+    result = (decimal % b).toString() + result;
     decimal = Math.floor(decimal / b);
 }
 
-console.log(result)
+console.log(result ? result : "0");
