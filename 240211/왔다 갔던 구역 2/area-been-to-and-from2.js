cb932c7c87bfd461ae36c48e6cd962726391b21c
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-11 17:23:05 +0900

    Title: 왔다 갔던 구역 2, Time: 188ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240211/왔다 갔던 구역 2/area-been-to-and-from2.js
+++ 240211/왔다 갔던 구역 2/area-been-to-and-from2.js
@@ -1,26 +1,29 @@
-const fs = require('fs')
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const testCases = input.slice(1).map(el => el.split(' '))
-const arr = []
-let current = 0
+const testCases = input.slice(1).map(el => el.split(' '));
+let current = 0;
+const arr = [];
 
 testCases.forEach(item => {
-    const [num, direction] = item;
+    const num = parseInt(item[0], 10);
+    const direction = item[1];
 
     if (direction === 'R') {
         for (let i = 0; i < num; i++) {
-            arr.push(current)
-            current++
+            current++;
+            arr.push(current);
         }
     } else {
         for (let i = 0; i < num; i++) {
-            arr.push(current)
-            current--
+            current--;
+            arr.push(current);
         }
     }
-})
+});
 
-const obj = {}
-arr.forEach(num => obj[num] ? obj[num]++ : obj[num] = 1)
-console.log(Object.keys(obj).filter(el => obj[el] >= 2).length)
+const obj = {};
+arr.forEach(num => obj[num] = (obj[num] || 0) + 1);
+const repeatedAreas = Object.values(obj).filter(count => count > 1);
+
+console.log(repeatedAreas.reduce((acc, count) => acc + count - 1, 0));
