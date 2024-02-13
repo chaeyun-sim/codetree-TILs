4e1552a6fef61b3ece360d9972f66b00b4e885c1
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-13 17:13:52 +0900

    Title: 연속되는 수 2, Time: 209ms, Memory: 14MB, Status: Passed - CodeTree

--- 240213/연속되는 수 2/continuous-number2.js
+++ 240213/연속되는 수 2/continuous-number2.js
@@ -1,19 +1,17 @@
-const fs = require('fs')
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const arr = input.slice(1).map(Number)
-let max = 0
-let cnt = 0
+let max = 1;
+let cnt = 1;
 
 for (let i = 1; i < arr.length; i++) {
-    cnt++
-    
-    if (arr[i] !== arr[i - 1]) {
-        if (cnt > max) {
-            max = cnt
-        }
-        cnt = 0
+    if (input[i] === input[i + 1]) {
+        cnt++;
+        max = Math.max(max, cnt);
+    } else {
+        cnt = 1;
     }
 }
 
-console.log(arr.length === 1 ? 1 : max)
+console.log(max);
