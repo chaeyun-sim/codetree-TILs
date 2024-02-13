8f5852e6b95dfd4754f6b2ed72d41aaf5eaf9a8d
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-13 18:54:43 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 204ms, Memory: 15MB, Status: Passed - CodeTree

--- 240213/T를 초과하는 연속 부분 수열/subsequence-above-t.js
+++ 240213/T를 초과하는 연속 부분 수열/subsequence-above-t.js
@@ -4,15 +4,15 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 const [n, t] = input[0].split(' ').map(Number)
 const arr = input[1].split(' ').map(Number)
 let max = 0
-let cnt = 1;
+let cnt = 0
 
-for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > 3 && arr[i + 1] > 3) {
-        cnt++
-        max = Math.max(max, cnt)
+for (let i = 0; i < n; i++) {
+    if (arr[i] > t) {
+        cnt++;
+        max = Math.max(max, cnt);
     } else {
-        cnt = 1
+        cnt = 0;
     }
 }
 
-console.log(max)
+console.log(max);
