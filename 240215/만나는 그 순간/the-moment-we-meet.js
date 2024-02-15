02ac0a7ebd8ebf2b63dd5b102dd6ed6a70ca30ce
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-15 15:18:12 +0900

    Title: 만나는 그 순간, Time: 259ms, Memory: 34MB, Status: Passed - CodeTree

--- 240215/만나는 그 순간/the-moment-we-meet.js
+++ 240215/만나는 그 순간/the-moment-we-meet.js
@@ -2,53 +2,49 @@ const fs = require('fs')
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
 const [n, m] = input[0].split(' ').map(Number)
-const arrA = input.slice(1, n + 1).map(el => el.split(' '))
-const arrB = input.slice(n + 1).map(el => el.split(' '))
-const distancesA = []
-const distancesB = []
-let currentA = 0
-let currentB = 0
+const testCasesA = input.slice(1, n + 1).map(el => el.split(' '))
+const testCasesB = input.slice(n + 1).map(el => el.split(' '))
+const arrA = [0]
+const arrB = [0]
 
-arrA.forEach(item => {
+testCasesA.forEach(item => {
     let [direction, distance] = item;
+    distance = Number(distance)
 
     while (distance > 0) {
-        if (distancesA.length) {
-            const lastItem = distancesA[distancesA.length - 1]
-            if (direction === 'R') {
-                distancesA.push(lastItem + 1)
-            } else {
-                distancesA.push(lastItem - 1)
-            }
+        const lastItem = arrA[arrA.length - 1]
+
+        if (direction === 'R') {
+            arrA.push(lastItem + 1)
         } else {
-            distancesA.push(0)
+            arrA.push(lastItem - 1)
         }
+
         distance--
     }
 })
 
-arrB.forEach(item => {
+testCasesB.forEach(item => {
     let [direction, distance] = item;
+    distance = Number(distance)
 
     while (distance > 0) {
-        if (distancesB.length) {
-            const lastItem = distancesB[distancesB.length - 1]
-            if (direction === 'R') {
-                distancesB.push(lastItem + 1)
-            } else {
-                distancesB.push(lastItem - 1)
-            }
+        const lastItem = arrB[arrB.length - 1]
+
+        if (direction === 'R') {
+            arrB.push(lastItem + 1)
         } else {
-            distancesB.push(0)
+            arrB.push(lastItem - 1)
         }
+        
         distance--
     }
 })
 
 const getWhenMeet = () => {
-    for (let i = 1; i < distancesA.length; i++) {
-        if (distancesB[i] === distancesA[i]) {
-            return i + 2
+    for (let i = 1; i < arrA.length; i++) {
+        if (arrB[i] === arrA[i]) {
+            return i
         }
     }
 
