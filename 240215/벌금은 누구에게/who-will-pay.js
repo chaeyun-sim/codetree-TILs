1a481f22922a7c3ea58880e02b7eac5e02cf4a0e
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-15 15:36:49 +0900

    Title: 벌금은 누구에게, Time: 186ms, Memory: 47MB, Status: Wrong Answer - CodeTree

---
+++ 240215/벌금은 누구에게/who-will-pay.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+
+const [n, m, k] = input[0].split(' ').map(Number)
+const students = input.slice(1).map(Number)
+const obj = {}
+
+for (let i = 0; i < students.length; i++) {
+    if (obj[students[i]]) {
+        obj[students[i]]++
+    } else {
+        obj[students[i]] = 1
+    }
+
+    if (obj[students[i]] >= k) {
+        console.log(students[i])
+        return;
+    }
+}
