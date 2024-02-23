edeef1101f6ce37713801d8c69887a43d24dafdc
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-23 13:32:16 +0900

    Title: 되돌아오기, Time: 254ms, Memory: 47MB, Status: Passed - CodeTree

--- 240223/되돌아오기/come-back.js
+++ 240223/되돌아오기/come-back.js
@@ -3,43 +3,38 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const n = Number(input[0]);
 const testCases = input.slice(1).map(el => el.split(' '));
-const dx = [0, 1, 0, -1]
-const dy = [1, 0, -1, 0]
+const dx = [0, 1, 0, -1];
+const dy = [1, 0, -1, 0];
+let target = Infinity;
 let x = 0, y = 0;
-let target = 0;
-let cnt = 0
+let cnt = 0;
 
-testCases.forEach((item, index) => {
-    const [direction, strDistance] = item;
-    let distance = Number(strDistance);
+for (let i = 0; i < n && target === Infinity; i++) {
+    const [direction, strDistance] = testCases[i];
+    const distance = Number(strDistance);
     let idx;
 
-    if (direction === 'N') {
-        idx = 0
-    } else if (direction === 'E') {
-        idx = 1
-    } else if (direction === 'S') {
-        idx = 2
-    } else if (direction === 'W') {
-        idx = 3
-    }
-    
-    while (distance > 0) {
+    if (direction === 'N') idx = 0;
+    else if (direction === 'E') idx = 1;
+    else if (direction === 'S') idx = 2;
+    else if (direction === 'W') idx = 3;
+
+    for (let j = 0; j < distance; j++) {
         x += dx[idx];
         y += dy[idx];
-
-        distance--
-        cnt++
+        cnt++;
 
         if (x === 0 && y === 0) {
-            target = cnt
+            target = cnt;
             break;
         }
     }
-});
 
-if (target)  {
-    console.log(target)
+    if (target !== Infinity) break;
+}
+
+if (target !== Infinity) {
+    console.log(target);
 } else {
-    console.log(-1)
+    console.log(-1);
 }
