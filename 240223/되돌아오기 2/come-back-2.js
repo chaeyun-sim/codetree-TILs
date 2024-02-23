e7f88f4688b5c593009840d9a08d779c999d2749
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-23 13:44:36 +0900

    Title: 되돌아오기 2, Time: 209ms, Memory: 19MB, Status: Wrong Answer - CodeTree

--- 240223/되돌아오기 2/come-back-2.js
+++ 240223/되돌아오기 2/come-back-2.js
@@ -1,42 +1,33 @@
-const fs = require('fs')
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const testCases =input[0].split('')
-let x = 0, y = 0
-const dx = [0, 1, 0, -1]
-const dy = [1, 0, -1, 0]
-let current = 0
-let cnt = 0
-let target = Infinity
+const testCases = input[0].split('');
+let x = 0, y = 0;
+const dx = [0, 1, 0, -1]; // 북, 동, 남, 서
+const dy = [1, 0, -1, 0];
+let current = 0;
+let cnt = 0;
+let target = Infinity;
 
-
-testCases.forEach(direction => {
+testCases.forEach((direction, index) => {
     if (direction === 'F') {
-        x += dx[current]
-        y += dy[current]
+        x += dx[current];
+        y += dy[current];
     } else if (direction === 'R') {
-        if (current === 3) {
-            current = 0
-        } else {
-            current++
-        }
+        current = (current + 1) % 4;
     } else if (direction === 'L') {
-        if (current === 0) {
-            current === 3
-        } else {
-            current--
-        }
+        current = (current + 3) % 4;
     }
-    cnt++
+    cnt++;
     
     if (x === 0 && y === 0) {
-        target = Math.min(target, cnt)
+        target = cnt;
         return;
     }
-})
+});
 
 if (target === Infinity) {
-    console.log(-1)
+    console.log(-1);
 } else {
-    console.log(target)
+    console.log(target);
 }
