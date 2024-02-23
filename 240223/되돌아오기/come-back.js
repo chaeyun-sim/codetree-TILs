04135554df7a8943f768c0600b87cd492b40d166
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-23 13:19:22 +0900

    Title: 되돌아오기, Time: 185ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240223/되돌아오기/come-back.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const n = Number(input[0]);
+const testCases = input.slice(1).map(el => el.split(' '));
+const dx = [0, 1, 0, -1]
+const dy = [1, 0, -1, 0]
+let x = 0, y = 0;
+let cnt = 0;
+
+testCases.forEach(item => {
+    const [direction, strDistance] = item;
+    let distance = Number(strDistance);
+    let idx;
+
+    if (direction === 'N') {
+        idx = 0
+    } else if (direction === 'E') {
+        idx = 1
+    } else if (direction === 'S') {
+        idx = 2
+    } else if (direction === 'W') {
+        idx = 3
+    }
+    
+    while (distance > 0) {
+        x += dx[idx];
+        y += dy[idx];
+
+        cnt++
+        distance--
+
+        if (x === 0 && y === 0) {
+            console.log(cnt)
+            return;
+        }
+    }
+});
