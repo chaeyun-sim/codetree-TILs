7623dcdf21a71000e44db6b1d4561ba8a39eff90
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-27 22:32:37 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 235ms, Memory: 15MB, Status: Passed - CodeTree

--- 240227/빙빙 돌며 사각형 채우기/snail-alphabet-square.js
+++ 240227/빙빙 돌며 사각형 채우기/snail-alphabet-square.js
@@ -5,14 +5,14 @@ let [n, m] = input[0].split(" ").map(Number);
 let x = 0, y = 0;
 const dx = [0, 1, 0, -1];
 const dy = [1, 0, -1, 0];
-const alphabet = Array(26).fill(0).map((_, i) => String.fromCharCode(i + 64))
+const alphabet = Array(26).fill(0).map((_, i) => String.fromCharCode(i + 65));
 let currentDirection = 0;
 const answer = Array(n).fill(null).map(() => Array(m).fill(0));
 answer[x][y] = 'A';
 
 const isInRange = (x, y) => 0 <= x && x < n && 0 <= y && y < m;
 
-for (let i = 2; i <= n * m; i++) {
+for (let i = 1; i < n * m; i++) {
     let nx = x + dx[currentDirection];
     let ny = y + dy[currentDirection];
 
@@ -24,7 +24,7 @@ for (let i = 2; i <= n * m; i++) {
 
     x = nx;
     y = ny;
-    answer[x][y] = alphabet[i > 26 ? 26 - i : i]
+    answer[x][y] = alphabet[(i % 26)];
 }
 
-answer.forEach(row => console.log(row.join(' ')))
+answer.forEach(row => console.log(row.join(' ')));
