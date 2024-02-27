83c608764b05bf05d7082796d07f73785d94b81d
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-27 22:30:16 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 192ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 240227/빙빙 돌며 사각형 채우기/snail-alphabet-square.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let [n, m] = input[0].split(" ").map(Number);
+let x = 0, y = 0;
+const dx = [0, 1, 0, -1];
+const dy = [1, 0, -1, 0];
+const alphabet = Array(26).fill(0).map((_, i) => String.fromCharCode(i + 64))
+let currentDirection = 0;
+const answer = Array(n).fill(null).map(() => Array(m).fill(0));
+answer[x][y] = 'A';
+
+const isInRange = (x, y) => 0 <= x && x < n && 0 <= y && y < m;
+
+for (let i = 2; i <= n * m; i++) {
+    let nx = x + dx[currentDirection];
+    let ny = y + dy[currentDirection];
+
+    if (!isInRange(nx, ny) || answer[nx][ny] !== 0) {
+        currentDirection = (currentDirection + 1) % 4;
+        nx = x + dx[currentDirection];
+        ny = y + dy[currentDirection];
+    }
+
+    x = nx;
+    y = ny;
+    answer[x][y] = alphabet[i > 26 ? 26 - i : i]
+}
+
+answer.forEach(row => console.log(row.join(' ')))
