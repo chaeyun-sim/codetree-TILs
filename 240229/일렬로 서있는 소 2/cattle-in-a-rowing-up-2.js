0883ae1af551473ea23a0aa583a999960a55b086
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-29 00:40:58 +0900

    Title: 일렬로 서있는 소 2, Time: 180ms, Memory: 15MB, Status: Passed - CodeTree

--- 240229/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.js
+++ 240229/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.js
@@ -8,7 +8,7 @@ let cnt = 0
 for (let i = 0; i < n; i++) {
     for (let j = i + 1; j < n; j++) {
         for (let k = j + 1; k < n; k++) {
-            if (arr[i] < arr[j] && arr[j] < arr[k]) {
+            if (arr[i] <= arr[j] && arr[j] <= arr[k]) {
                 cnt++
             }
         }
