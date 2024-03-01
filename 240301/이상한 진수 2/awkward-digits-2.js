22c2a08a923f4c31e243fb003228f45f49850037
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-01 13:22:29 +0900

    Title: 이상한 진수 2, Time: 185ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240301/이상한 진수 2/awkward-digits-2.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+
+const binary = input[0]
+let max = Number(binary)
+
+for (let i = 0; i < binary.length; i++) {
+    const left = binary.slice(0, i)
+    const right = binary.slice(i + 1)
+    const convert = binary[i] === '1' ? '0' : '1'
+    const result = left + convert + right
+
+    if (max < Number(result)) {
+        max = result
+    }
+}
+
+console.log(parseInt(max, 2))
