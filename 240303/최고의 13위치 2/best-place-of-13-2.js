abb09070a64a5b4de0607be48820584ba5aabbf9
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-03 12:17:01 +0900

    Title: 최고의 13위치 2, Time: 176ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240303/최고의 13위치 2/best-place-of-13-2.js
+++ 240303/최고의 13위치 2/best-place-of-13-2.js
@@ -3,14 +3,18 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
 const n = Number(input[0])
 const arr = input.slice(1).map(el => el.split(' ').map(Number))
-const list = []
+const obj = {}
 
 
 for (let i = 0; i < n; i++) {
     for (let j = 0; j < n - 2; j++) {
-        list.push(arr[i][j] + arr[i][j + 1] + arr[i][j + 2])
+        if (!obj[i]) {
+            obj[i] = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+        } else {
+            obj[i] = Math.max(obj[i], arr[i][j] + arr[i][j + 1] + arr[i][j + 2])
+        }
     }
 }
 
-const sorted = list.sort((a, b) => b - a)
+const sorted = Object.values(obj).sort((a, b) => b - a)
 console.log(sorted[0] + sorted[1])
