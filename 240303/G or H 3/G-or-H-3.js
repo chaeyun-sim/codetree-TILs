aa7051bbe9c42f73101521365f7f40d27548b69a
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-03 13:34:05 +0900

    Title: G or H 3, Time: 172ms, Memory: 15MB, Status: Passed - CodeTree

--- 240303/G or H 3/G-or-H-3.js
+++ 240303/G or H 3/G-or-H-3.js
@@ -4,24 +4,15 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 const [n, k] = input[0].split(' ').map(Number)
 const arr = input.slice(1).map(el => el.split(' '))
 arr.sort((a, b) => Number(a[0]) - Number(b[0]))
-const list = []
+
+let max = 0;
 
 for (let i = 0; i < n; i++) {
-    for (let j = i + 1; j < n; j++) {
-        if (Number(arr[j][0]) - Number(arr[i][0]) === k) {
-            list.push(arr.slice(i, j + 1).map(el => el[1]))
-        }
+    let score = 0;
+    for (let j = i; j < n && arr[j][0] - arr[i][0] <= k; j++) {
+        score += arr[j][1] === 'G' ? 1 : 2;
     }
+    max = Math.max(max, score);
 }
 
-let max = 0
-list.forEach(item => {
-    const sum = item.map(el => el === 'G' ? 1 : 2).reduce((a, b) => a + b)
-    max = Math.max(max, sum)
-})
-
-if (arr.length === 1) {
-    console.log(arr[0][0] - 1)
-} else {
-    console.log(max)
-}
+console.log(max);
