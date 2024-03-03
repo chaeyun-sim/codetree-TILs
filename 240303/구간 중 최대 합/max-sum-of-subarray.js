8061af0b0d07be68eb321ec332799ae48c6360e2
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-03 13:04:38 +0900

    Title: 구간 중 최대 합, Time: 180ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240303/구간 중 최대 합/max-sum-of-subarray.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+
+const [n, k] = input[0].split(' ').map(Number)
+const arr = input[1].split(' ').map(Number)
+let max = -Infinity;
+
+for (let i = 0; i < n - k; i++) {
+    const sliced = arr.slice(i, i + k)
+    max = Math.max(sliced.reduce((a, b) => a + b), max)
+}
+
+console.log(max)
