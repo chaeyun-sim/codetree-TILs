0ed1db8c9417a3c5c2921243972bd74398afbe1b
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-03 13:05:35 +0900

    Title: 구간 중 최대 합, Time: 178ms, Memory: 14MB, Status: Passed - CodeTree

--- 240303/구간 중 최대 합/max-sum-of-subarray.js
+++ 240303/구간 중 최대 합/max-sum-of-subarray.js
@@ -5,9 +5,10 @@ const [n, k] = input[0].split(' ').map(Number)
 const arr = input[1].split(' ').map(Number)
 let max = -Infinity;
 
-for (let i = 0; i < n - k; i++) {
+for (let i = 0; i <= n - k; i++) {
     const sliced = arr.slice(i, i + k)
-    max = Math.max(sliced.reduce((a, b) => a + b), max)
+    const sum = sliced.reduce((a, b) => a + b)
+    max = Math.max(max, sum)
 }
 
 console.log(max)
