742c9135136909ba7766814fa26b311e9b9767f0
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-07 13:52:52 +0900

    Title: G or H 2, Time: 204ms, Memory: 15MB, Status: Passed - CodeTree

--- 240307/G or H 2/G-or-H-2.js
+++ 240307/G or H 2/G-or-H-2.js
@@ -17,7 +17,7 @@ function checkEqual(arr) {
 }
 
 for (let i = 0; i <= n; i++) {
-    for (let j = i + 1; j < n; j++) {
+    for (let j = i + 1; j <= n; j++) {
         const newArr = arr.slice(i, j)
         if (checkEqual(newArr)) {
             const num = Number(newArr[newArr.length - 1][0]) - Number(newArr[0][0])
@@ -26,4 +26,8 @@ for (let i = 0; i <= n; i++) {
     }
 }
 
-console.log(max)
+if (arr.length === 1) {
+    console.log(0)
+} else {
+    console.log(max)
+}
