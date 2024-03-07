c009af82b83a02ced907a4000740cba477e7e90b
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-07 14:09:48 +0900

    Title: 밭의 높이를 고르게하기, Time: 201ms, Memory: 15MB, Status: Passed - CodeTree

--- 240307/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.js
+++ 240307/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.js
@@ -1,24 +1,21 @@
-const fs = require("fs")
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+const fs = require("fs");
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const [n, h, t] = input[0].split(' ').map(Number)
-const arr = input[1].split(' ').map(Number)
+const [n, h, t] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
 let min = Infinity;
 
-function calculate(arr) {
-    let sum = 0
-    for (let i = 0; i < t; i++) {
-        sum += Math.abs(t - arr[i])
+function calculateCost(startIndex) {
+    let sum = 0;
+    for (let i = startIndex; i < startIndex + t; i++) {
+        sum += Math.abs(h - arr[i])
     }
-
-    return sum
+    return sum;
 }
 
-for (let i = 0; i < n - t + 1; i++) {
-    const newArr = arr.slice(i, i + t)
-    if (calculate(newArr) <= h) {
-        min = Math.min(min, calculate(newArr))
-    }
+for (let i = 0; i <= n - t; i++) {
+    const sum = calculateCost(i);
+    min = Math.min(min, sum);
 }
 
-console.log(min)
+console.log(min);
