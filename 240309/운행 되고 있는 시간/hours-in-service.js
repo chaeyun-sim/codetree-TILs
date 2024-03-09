c116cdff6c24a75c94a6b49d34712c946bc38796
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-09 18:53:22 +0900

    Title: 운행 되고 있는 시간, Time: 195ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 240309/운행 되고 있는 시간/hours-in-service.js
+++ 240309/운행 되고 있는 시간/hours-in-service.js
@@ -3,32 +3,24 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
 const n = Number(input[0])
 const arr = input.slice(1).map(el => el.split(' ').map(Number))
-let min = Infinity;
+let max = 0
 
-function getTime(arr) {
+function checkTime(arr) {
     const obj = {}
 
     for (let i = 0; i < arr.length; i++) {
-        let [start,end] = arr[i];
-        while (start <= end) {
-            if (obj[start]) {
-                obj[start]++
-            } else {
-                obj[start] = 1
-            }
-            start++
+        for (let j = arr[i][0]; j <= arr[i][1]; j++) {
+            if (!obj[j]) obj[j] = 1
         }
     }
 
-    return obj
+    return Object.keys(obj).length
 }
+
 
 for (let i = 0; i < n; i++) {
-    const newArr = arr.slice(0, i).concat(arr.slice(i + 1))
-    const works = getTime(newArr)
-    const keys = Object.keys(works).map(Number)
-    const workingTime = keys[keys.length - 1] - keys[0] + 1
-    min = Math.min(workingTime, min)
+    const newArr = arr.slice(0, i).concat(arr.slice(i + 1)).sort((a, b) => a[0] - b[0])
+    max = Math.max(max, checkTime(newArr) - 2)
 }
 
-console.log(min)
+console.log(max)
