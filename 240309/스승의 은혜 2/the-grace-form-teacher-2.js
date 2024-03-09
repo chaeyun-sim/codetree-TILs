e1c119358a52215d4cef403084c954bcfd5d66d7
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-09 22:56:50 +0900

    Title: 스승의 은혜 2, Time: 205ms, Memory: 18MB, Status: Passed - CodeTree

--- 240309/스승의 은혜 2/the-grace-form-teacher-2.js
+++ 240309/스승의 은혜 2/the-grace-form-teacher-2.js
@@ -3,19 +3,25 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
 const [n, b] = input[0].split(' ').map(Number)
 const arr = input.slice(1).map(Number)
-let max = 0
+arr.sort((a, b) => a - b);
 
-for (let i = 0; i <= n; i++) {
-    for (let j = i + 1; j <= n; j++) {
-        const newArr = arr.slice(i, j)
-        for (let k = 0; k < newArr.length; k++) {
-            const list = [...newArr.slice(0, k), newArr[k] / 2, ...newArr.slice(k + 1)]
-            const sum = list.reduce((a,b) => a + b)
-            if (sum <= b) {
-                max = Math.max(list.length, max)
-            }
+let max = 0;
+
+for (let i = 0; i < n; i++) {
+    let budget = b;
+    let count = 0;
+
+    budget -= arr[i] / 2;
+    count++;
+
+    for (let j = 0; j < n; j++) {
+        if (i !== j && budget >= arr[j]) {
+            budget -= arr[j];
+            count++;
         }
     }
+
+    max = Math.max(max, count);
 }
 
-console.log(max)
+console.log(max);
