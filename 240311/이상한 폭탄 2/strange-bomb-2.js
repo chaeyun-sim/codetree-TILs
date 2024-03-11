1436027761a4a1a10d2b8c901fabf96a0c944046
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-11 23:18:20 +0900

    Title: 이상한 폭탄 2, Time: 169ms, Memory: 14MB, Status: Passed - CodeTree

--- 240311/이상한 폭탄 2/strange-bomb-2.js
+++ 240311/이상한 폭탄 2/strange-bomb-2.js
@@ -1,19 +1,22 @@
-const fs = require('fs')
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const [n, k] = input[0].split(' ').map(Number)
-const arr = input.slice(1).map(Number)
-let flag = false
-let max = 0
-let cnt = 0
+const [n, k] = input[0].split(' ').map(Number);
+const arr = input.slice(1).map(Number);
+let map = new Map();
+let max = -1;
 
 for (let i = 0; i < n; i++) {
-    if (flag) {
-        max = Math.max(max, arr[i])
-        cnt++
-    }
-    
-    if (arr[i] === 3) flag = !flag
+    map.set(arr[i], map.has(arr[i]) ? [...map.get(arr[i]), i] : [i])
 }
 
-console.log(cnt <= k ? max : -1)
+map.forEach((item, number) => {
+    for (let i = 1; i < item.length; i++) {
+        if (item[i] - item[i - 1] <= k) {
+            max = Math.max(max, number);
+            break;
+        }
+    }
+});
+
+console.log(max);
