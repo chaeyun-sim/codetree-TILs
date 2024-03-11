9261f855c929a8dfb4dcd2bd6c92f0b1c4d6edff
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-12 00:54:58 +0900

    Title: 스승의 은혜 3, Time: 177ms, Memory: 47MB, Status: Passed - CodeTree

--- 240312/스승의 은혜 3/the-grace-form-teacher-3.js
+++ 240312/스승의 은혜 3/the-grace-form-teacher-3.js
@@ -1,29 +1,26 @@
 const fs = require('fs')
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
-const [n, b] = input[0].split(' ').map(Number)
-const arr = input.slice(1).map(el => el.split(' ').map(Number))
-arr.sort((a, b) => (a[0] + a[1]) - (b[0] + b[1]))
-let max = 0
+const [n, b] = input[0].split(' ').map(Number);
+const arr = input.slice(1).map(el => el.split(' ').map(Number));
+arr.sort((a, b) => (a[0] + a[1]) - (b[0] + b[1]));
+let max = 0;
 
 for (let i = 0; i < n; i++) {
-    let budget = b
-    let cnt = 1;
-    const currentDiscount = arr[i][0] / 2 + arr[i][1]
-
-    if (currentDiscount <= budget) {
-        budget -= currentDiscount
-    }
+    let budget = b - (arr[i][0] / 2 + arr[i][1]);
+    let cnt = budget >= 0 ? 1 : 0;
 
     for (let j = 0; j < n; j++) {
-        const sum = arr[j].reduce((a, b) => a + b)
-        if (i !== j && sum <= budget) {
-            budget -= sum
-            cnt++;
+        if (i !== j) {
+            const sum = arr[j][0] + arr[j][1];
+            if (sum <= budget) {
+                budget -= sum;
+                cnt++;
+            }
         }
     }
 
-    max = Math.max(max, cnt)
+    max = Math.max(max, cnt);
 }
 
-console.log(max)
+console.log(max);
