2212c29b35279a97882007811fb4f789d5085620
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-11 22:54:26 +0900

    Title: 이상한 폭탄 2, Time: 164ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240311/이상한 폭탄 2/strange-bomb-2.js
+++ 240311/이상한 폭탄 2/strange-bomb-2.js
@@ -5,13 +5,15 @@ const [n, k] = input[0].split(' ').map(Number)
 const arr = input.slice(1).map(Number)
 let flag = false
 let max = 0
+let cnt = 0
 
 for (let i = 0; i < n; i++) {
     if (flag) {
         max = Math.max(max, arr[i])
+        cnt++
     }
     
     if (arr[i] === 3) flag = !flag
 }
 
-console.log(max || -1)
+console.log(cnt < k ? max : -1)
