34f16f11529c4b1bbeee574e0b1b91632de866c3
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-12 00:54:21 +0900

    Title: 스승의 은혜 3, Time: 196ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 240312/스승의 은혜 3/the-grace-form-teacher-3.js
+++ 240312/스승의 은혜 3/the-grace-form-teacher-3.js
@@ -3,18 +3,22 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
 const [n, b] = input[0].split(' ').map(Number)
 const arr = input.slice(1).map(el => el.split(' ').map(Number))
-arr.sort((a, b) => a[1] - b[1] || b[0] - a[0])
+arr.sort((a, b) => (a[0] + a[1]) - (b[0] + b[1]))
 let max = 0
 
 for (let i = 0; i < n; i++) {
+    let budget = b
     let cnt = 1;
-    const discount = arr[i][0] / 2 + arr[i][1]
-    let total = discount <= b ? discount : 0
+    const currentDiscount = arr[i][0] / 2 + arr[i][1]
+
+    if (currentDiscount <= budget) {
+        budget -= currentDiscount
+    }
 
     for (let j = 0; j < n; j++) {
         const sum = arr[j].reduce((a, b) => a + b)
-        if (i !== j && total + sum <= b) {
-            total += sum;
+        if (i !== j && sum <= budget) {
+            budget -= sum
             cnt++;
         }
     }
