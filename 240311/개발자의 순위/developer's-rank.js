35df7b168528915c36391d5c3bacdb6bc28b8835
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-11 22:24:10 +0900

    Title: 개발자의 순위, Time: 182ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240311/개발자의 순위/developer's-rank.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+
+const [k, n] = input[0].split(' ').map(Number)
+const arr = input.slice(1).map(el => el.split(' ').map(Number))
+const obj = {}
+
+for (let i = 0; i < k; i++) {
+    for (let j = 0; j < n; j++) {
+        for (let k = j + 1; k < n; k++) {
+            let number = String(arr[i][j]) + String(arr[i][k])
+            obj[number] = (obj[number] || 0) + 1
+        }
+    }
+}
+
+const filtered = Object.values(obj).filter(el => el === k)
+console.log(filtered.length)
