88ea1d66cc5bfe23d7c25e0f533d7116329cf604
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-11 23:51:30 +0900

    Title: 선분 3개 지우기, Time: 214ms, Memory: 15MB, Status: Passed - CodeTree

--- 240311/선분 3개 지우기/remove-three-segments.js
+++ 240311/선분 3개 지우기/remove-three-segments.js
@@ -3,14 +3,25 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
 const n = Number(input[0])
 const arr = input.slice(1).map(el => el.split(' ').map(Number)).sort((a, b) => a[0] - b[0] || a[1] - b[1])
-const answer = [[arr[0]]]
+let cnt = 0
 
-for (let i = 1; i < n; i++) {
-    if (arr[i - 1][1] > arr[i][0]) {
-        answer[answer.length - 1].push(arr[i])
-    } else {
-        answer.push([arr[i]])
+function isOverlapped(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i][0] <= arr[i - 1][1]) {
+            return true
+        }
+    }
+
+    return false
+}
+
+for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+        for (let k = j + 1; k < n; k++) {
+            const newArr = arr.filter((_, idx) => idx !== i && idx !== j && idx !== k)
+            if (!isOverlapped(newArr)) cnt++
+        }
     }
 }
 
-console.log(answer.map(el => el.length).reduce((a, b) => a * b))
+console.log(cnt)
