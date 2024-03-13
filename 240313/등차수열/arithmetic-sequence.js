0d32e7946503a46e04f72cec71208210687a69c2
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-13 23:23:32 +0900

    Title: 등차수열, Time: 248ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 240313/등차수열/arithmetic-sequence.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+
+const n = Number(input[0])
+const arr = input[1].split(' ').map(Number).sort((a, b) => a - b)
+let cnt = 0
+
+for (let i = 1; i <= 100; i++) {
+    let num = i;
+
+    for (let j = 0; j < n; j++) {
+        for (let k = j + 1; k < n; k++) {
+            if (num - arr[j] === arr[k] - num) {
+                cnt++
+            }
+        }
+    }
+}
+
+console.log(cnt)
