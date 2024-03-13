a32412f9a104ef7cfd9975dd96ef260673b90997
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-13 23:33:49 +0900

    Title: 등차수열, Time: 179ms, Memory: 17MB, Status: Passed - CodeTree

--- 240313/등차수열/arithmetic-sequence.js
+++ 240313/등차수열/arithmetic-sequence.js
@@ -2,20 +2,21 @@ const fs = require('fs')
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
 const n = Number(input[0])
-const arr = input[1].split(' ').map(Number).sort((a, b) => a - b)
-const map = new Map();
+const arr = input[1].split(' ').map(Number)
 let max = 0;
 
-for (let i = 0; i < n; i++) {
-    for (let j = i + 1; j < n; j++) {
-      const k = (arr[j] + arr[i]) / 2.0;
-      
-      if (Number.isInteger(k)) {
-        const item = map.get(k)
-        map.set(k, (item || 0) + 1);
-        max = Math.max(max, item);
-      }
+for (let i = 1; i <= 100; i++) {
+    let cnt = 0
+
+    for (let j = 0; j < n; j++) {
+        for (let k = j + 1; k < n; k++) {
+            if (arr[j] + arr[k] === 2 * i) {
+                cnt++
+            }
+        }
     }
+
+    max = Math.max(max, cnt)
 }
 
 console.log(max)
