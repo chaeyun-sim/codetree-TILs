044d40ec1ff0fc285898a65f0676ed8c2d872a1e
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-14 16:33:05 +0900

    Title: 삼각형 만들기, Time: 333ms, Memory: 18MB, Status: Passed - CodeTree

--- 240314/삼각형 만들기/create-triangle.js
+++ 240314/삼각형 만들기/create-triangle.js
@@ -6,20 +6,20 @@ const arr = input.slice(1).map(el => el.split(' ').map(Number))
 let max = 0
 
 for (let i = 0; i < n; i++) {
-    for (let j = i + 1; j < n; j++) {
-        for (let k = j + 1; k < n; k++) {
-            const [x1, y1] = arr[i]
-            const [x2, y2] = arr[j]
-            const [x3, y3] = arr[k]
-
-            if (y1 === y2 || y2 === y3 || y1 === y3 || x1 === x2 || x2 === x3 || x1 === x3) {
-                const sum = Math.abs((x1 * y2 + x2 * y3 + x3 * y1) - (x2 * y1 + x3 * y2 + x1 * y3));
-                max = Math.max(max, sum);
+    for (let j = 0; j < n; j++) {
+        for (let k = 0; k < n; k++) {
+            if (i !== j && i !== k && j !== k) {
+                const [x1, y1] = arr[i];
+                const [x2, y2] = arr[j];
+                const [x3, y3] = arr[k];
+                
+                if ((x1 === x2 && y2 === y3) || (x2 === x3 && y3 === y1) || (x3 === x1 && y1 === y2)) {
+                    let area = Math.abs(x1 - x2) * Math.abs(y1 - y2);
+                    max = Math.max(max, area);
+                }
             }
-
-            
         }
     }
 }
 
-console.log(max)
+console.log(max);
