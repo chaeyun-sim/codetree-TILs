2da8de08743306a64748f68e9ca38a2fc2d53516
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-21 22:59:22 +0900

    Title: 두 선분, Time: 169ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240321/두 선분/two-lines.js
+++ 240321/두 선분/two-lines.js
@@ -4,8 +4,8 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 const [x1, x2, x3, x4] = input[0].split(' ').map(Number)
 let flag = false
 
-if (x2 => x3 || (x2 >= x4 && x1 <= x3)) {
+if (x2 < x3) {
     flag = true
 }
 
-console.log(flag ? 'intersecting' : 'nonintersecting')
+console.log(flag ? 'nonintersecting' : 'intersecting')
