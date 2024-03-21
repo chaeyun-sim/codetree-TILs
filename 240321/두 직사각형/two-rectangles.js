4a6d540802b5333e0e4a232670c060a1310b8db5
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-03-21 23:19:10 +0900

    Title: 두 직사각형, Time: 312ms, Memory: 14MB, Status: Passed - CodeTree

--- 240321/두 직사각형/two-rectangles.js
+++ 240321/두 직사각형/two-rectangles.js
@@ -2,10 +2,10 @@ const fs = require('fs')
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
 const [x1, y1, x2, y2] = input[0].split(' ').map(Number)
-const [a1, b1, a2, b2] = input[0].split(' ').map(Number)
+const [a1, b1, a2, b2] = input[1].split(' ').map(Number)
 let flag = false
 
-if (x2 <= a1 || a2 <= x1 || b2 <= y1 || y2 <= b1) {
+if (x2 < a1 || x1 > a2 || y2 < b1 || y1 > b2) {
     flag = true
 }
 
